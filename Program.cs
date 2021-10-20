c46fad9f6e2b36270ceba08763fa7a03a5b47266
heathercodd <heathercodd@yahoo.co.uk>
2021-10-20 22:36:39 +0100

    connect to sql

--- Program.cs
+++ Program.cs
@@ -1,5 +1,7 @@
 ﻿using CSharp_Project.Controllers;
 using CSharp_Project.Menus;
+using CSharp_Project.Utils;
+using MySql.Data.MySqlClient;
 using System;
 
 namespace CSharp_Project
@@ -11,10 +13,19 @@ namespace CSharp_Project
             //console text will be yellow
             Console.ForegroundColor = ConsoleColor.Yellow;
 
+            /*
             //runs the main menu 
             SaleMenu myMenu = new SaleMenu(new SaleController());
             myMenu.RunMainMenu();
 
+            
+            //to create my sale table in the database 
+            MySqlConnection connection = MySqlUtils.GetSqlConnection();
+            connection.Open();
+            MySqlUtils.RunSchemaFile(Environment.CurrentDirectory + @"\Resources\schema.sql", connection);
+            connection.Dispose();
+
+            */
 
 
         }
--- Utils/MySqlUtils.cs
+++ Utils/MySqlUtils.cs
@@ -3,10 +3,45 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using MySql.Data.MySqlClient;
 
 namespace CSharp_Project.Utils
 {
     class MySqlUtils
     {
+        //building the connection to the database
+        public static MySqlConnectionStringBuilder MyConnectionString { get; set; } = new MySqlConnectionStringBuilder
+        {
+            //details to connect to
+            Server = "127.0.0.1",
+            UserID = "root",
+            Password = "root",
+            Port = 3306,
+            Database = "sales",
+            AllowBatch = true, //batches of com mands can be sent
+            AllowLoadLocalInfileInPath = "./", //only files in this directory can be uploaded
+            AllowLoadLocalInfile = false, //files cannot be uploaded from anywhere
+            ConnectionTimeout = 30 //30 seconds until timeout error
+        };
+
+        //connecting to the database
+        public static MySqlConnection GetSqlConnection()
+        {
+            return new MySqlConnection(MyConnectionString.ConnectionString);
+        }
+
+
+        //command to run all text from the provided filepath in sql 
+        public static void RunSchemaFile(string filepath, MySqlConnection myConnection)
+        {
+            string schemafile = File.ReadAllText(filepath);
+            MySqlCommand mySqlCommand = myConnection.CreateCommand();
+            mySqlCommand.CommandText = schemafile;
+            mySqlCommand.ExecuteNonQuery();
+
+        }
+
+
     }
 }
